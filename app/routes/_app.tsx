ac6156eee2bf5396e935618b500b394b88c27bf3
LoreviQ <lorevi@protonmail.com>
2025-02-18 20:43:15 +0000

    Simplify App route component structure

--- app/routes/_app.tsx
+++ app/routes/_app.tsx
@@ -4,12 +4,6 @@ import { Header } from "~/components/header";
 
 export default function App() {
     return (
-        <Layout />
-    );
-}
-
-function Layout() {
-    return (
         <div className="min-h-screen bg-linear-to-b from-theme-bg to-theme-bg-secondary text-white">
             <Header />
             <div className="mx-auto">
